baab6e52c2143c5e6faf41617edd3e9a014e5cd7
exis <gotexis@gmail.com>
2021-12-09 14:38:20 +1100

    fin

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,23 @@
 import { httpGet } from './mock-http-interface';
 
-// TODO define this type properly
-type TResult = any;
+type SuccessResult = {
+  'Arnie Quote': string
+}
+type FailureResult = {
+  FAILURE: string
+}
+type TResult = SuccessResult | FailureResult;
 
-export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
-  // TODO: Implement this function.
-  
-  return [];
+export const getArnieQuotes = async (urls: string[]): Promise<TResult[]> => {
+  const results = await Promise.all(urls.map(async url => {
+    const getOneQuote = await httpGet(url)
+    const msg = JSON.parse(getOneQuote.body).message
+    if (getOneQuote.status === 200) {
+      return { "Arnie Quote": msg };
+    } else {
+      return { FAILURE: msg };
+    }
+  })
+  )
+  return results;
 };
